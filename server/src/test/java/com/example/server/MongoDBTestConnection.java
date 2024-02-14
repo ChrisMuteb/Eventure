34f15995abc03e1e79c897e177f1cf87de7c39c2
chriskayomb <ibond.chris@gmail.com>
2024-02-14 20:23:39 +0100

    Eventure: Initial commit

---
+++ server/src/test/java/com/example/server/MongoDBTestConnection.java
@@ -0,0 +1,19 @@
+package com.example.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataMongoTest
+public class MongoDBTestConnection {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    @Test
+    public void testMongoDBConnection(){
+        assertTrue(mongoTemplate != null);
+//        assertTrue(mongoTemplate.collectionExists("person"));
+    }
+}
